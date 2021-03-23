8329be98e66bbdbbb85f9d836b9b05fa80e94fae
Ngoc Van Duong <van-ngoc.duong@ekino.com>
2021-03-23 17:14:38 +0700

    feat:create post form/express post requests/ saving on database/ display list all posts

--- index.js
+++ index.js
@@ -1,17 +1,46 @@
 const path = require('path')
 const express = require('express')
-const { config, engine } = require('express-edge');
+const { engine } = require('express-edge')
+const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
+const Post = require('./database/models/Post')
 
 const app = new express()
+
+mongoose.connect('mongodb://localhost/build-blog-with-nodejs', {useNewUrlParser: true, useUnifiedTopology: true})
 
 app.use(express.static('public'))
 
 app.use(engine)
 
+app.use(bodyParser.json())
+
+app.use(bodyParser.urlencoded({extended: true}))
+
 app.set('views', `${__dirname}/views`)
 
-app.get('/', (req, res) => {
-    res.render('index')
+app.get('/', async (req, res) => {
+    const posts = await Post.find({})
+
+    console.log(posts);
+    res.render('index', {
+        posts
+    })
+})
+
+app.get('/posts/new', (req, res) => {
+    res.render('create')
+})
+
+app.post('/posts/store', (req,res) => {
+
+    console.log(req.body);
+
+    Post.create(req.body, (error, post) => {
+        
+        res.redirect('/')
+    })
+
 })
 
 app.get('/about', (req, res) => {
