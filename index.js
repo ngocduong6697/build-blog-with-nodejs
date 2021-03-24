f2b429bdba4866b2867f774ae93e895bda35e5c0
Ngoc Van Duong <van-ngoc.duong@ekino.com>
2021-03-24 11:07:56 +0700

    feat: registering validation middleware

--- index.js
+++ index.js
@@ -20,6 +20,14 @@ app.use(bodyParser.json())
 
 app.use(bodyParser.urlencoded({extended: true}))
 
+const validateCreatePostMiddleware = (req, res, next) => {
+    if(!req.files.image || !req.body.username || !req.body.title || !req.body.subtitle || !req.body.content) {
+        return res.redirect('posts/new')
+    }
+    next()
+}
+
+app.use('/posts/store', validateCreatePostMiddleware)
 
 app.set('views', `${__dirname}/views`)
 
