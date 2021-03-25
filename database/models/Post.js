48138b8a476b51d6ea281b2efba729a948befde3
Ngoc Van Duong <van-ngoc.duong@ekino.com>
2021-03-25 11:18:23 +0700

    feat: display post with user data

--- database/models/Post.js
+++ database/models/Post.js
@@ -4,6 +4,11 @@ const PostSchema = new mongoose.Schema({
     title: String,
     subtitle: String,
     content: String,
+    author: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
     username: String,
     image: String,
     createAt: {
